0d94ae62bff3ac8fadc899d63c0a5c488e9c0714
SHUBHAM BISWAS <suvm.dev@gmail.com>
2024-01-17 04:43:29 +0530

    learn Variables in Swift

--- Variables.playground/Contents.swift
+++ Variables.playground/Contents.swift
@@ -1,3 +1,32 @@
-import UIKit
+import Foundation
 
-var greeting = "Hello, playground"
+let myName = "Shubham"
+let yourName = "Foo"
+
+var names = [
+    myName,
+    yourName
+]
+
+names.append("Bar")
+names.append("Baz")
+
+
+let foo = "Foo"
+var foo2 = foo
+
+
+let moreNames = [
+"Foo",
+"Bar"
+]
+
+var copy = moreNames
+
+copy.append("Baz")
+
+moreNames
+copy
+
+
+
